93e8fdf41f76d913d41e896e2af742765ab016d9
ghouili <walid.ghouili98@gmail.com>
2026-02-09 18:43:19 +0100

    updates 09-02-2026 07

--- lib/config.ts
+++ lib/config.ts
@@ -7,7 +7,7 @@ export const siteConfig = {
   url: "https://debarras-aurea.fr",
 
   contact: {
-    phone: " 7 63 69 98 49",
+    phone: " 07 63 69 98 49",
     email: "contact@debarras-aurea.fr",
     address: "Île-de-France",
   },
